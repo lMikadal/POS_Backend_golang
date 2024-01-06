4bba55c79b8bb70beb1215a5805df5bbefa983ee
Mikada <panupong.mikada@gmail.com>
2024-01-06 22:10:44 +0700

    update first get all user

--- controllers/user_c.go
+++ controllers/user_c.go
@@ -4,10 +4,15 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lMikadal/POS_Backend_golang.git/models"
 )
 
 func (db *DBController) GetUser(c *gin.Context) {
+	var users []models.User
+
+	db.Database.Find(&users)
+
 	c.JSON(http.StatusOK, gin.H{
-		"message": "user",
+		"users": users,
 	})
 }
