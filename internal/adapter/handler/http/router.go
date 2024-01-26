ad135fb013e8d9c54ef681591fd41d15c9cb48b4
Mikada <panupong.mikada@gmail.com>
2024-01-26 22:29:13 +0700

    add header

--- internal/adapter/handler/http/router.go
+++ internal/adapter/handler/http/router.go
@@ -29,7 +29,7 @@ func NewRouter(
 	}
 
 	router := gin.New()
-	router.Use(gin.LoggerWithFormatter(customLogger), gin.Recovery())
+	router.Use(gin.LoggerWithFormatter(customLogger), gin.Recovery(), CORSMiddleware())
 	v1 := router.Group("api/v1")
 	{
 		user := v1.Group("/users")
@@ -82,4 +82,20 @@ func customLogger(param gin.LogFormatterParams) string {
 // Serve starts the HTTP server
 func (r *Router) Serve(listenAddr string) error {
 	return r.Run(listenAddr)
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
